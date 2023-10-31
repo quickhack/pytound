ff83f3f3aafc12dde6151fa5a98dbeb9abd18c92
Jerry Lee <oldratlee@gmail.com>
2023-10-31 19:25:06 +0800

    build/reactor: extract var `proj_py_files` in `lint.sh` 🛠️

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,13 +4,15 @@ set -euo pipefail
 cd "$(dirname "$(readlink -f "$0")")/.."
 source scripts/_utils.sh
 
-log_and_run flake8 --count --statistics --show-source --max-complexity=10 --max-line-length=120 pytound/ tests/
+readonly proj_py_files=(pytound/ tests/)
 
-log_and_run isort --line-length=120 --check-only --diff pytound/ tests/
+log_and_run flake8 --count --statistics --show-source --max-complexity=10 --max-line-length=120 "${proj_py_files[@]}"
+
+log_and_run isort --line-length=120 --check-only --diff "${proj_py_files[@]}"
 
 # https://mypy.readthedocs.io/en/stable/running_mypy.html#missing-imports
 # https://mypy.readthedocs.io/en/stable/existing_code.html#ignoring-errors-from-certain-modules
-log_and_run mypy pytound/ tests/
+log_and_run mypy "${proj_py_files[@]}"
 
 # show --strict mypy info only without fail lint
-log_and_run mypy --strict pytound/ tests/ || true
+log_and_run mypy --strict "${proj_py_files[@]}" || true
